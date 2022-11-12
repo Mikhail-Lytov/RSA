8d4071318069abfbe212f3b1be26898b918dbb86
Mikhail-Lytov <Misha.Lytov@yandex.ru>
2022-11-12 04:04:49 +0300

    key generation

---
+++ src/key_generation.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class key_generation {
+    private long first_element;
+    private long second_element;
+    private long derivative;
+    private long euler_function;
+    private long e;
+    private long lcm_element;
+    private long open_exhibitor;
+    private long close_exhibitor;
+    public key_generation(long first_element, long second_element){
+        this.first_element = first_element;
+        this.second_element = second_element;
+        this.derivative = first_element * second_element;
+        this.euler_function = (first_element - 1) * (second_element - 1);
+        this.open_exhibitor = open_exhibitor();
+        this.close_exhibitor = close_exhibitor();
+
+    }
+    private long close_exhibitor(){
+        long close_exhibitor = 1;
+        while (true){
+            if((close_exhibitor * open_exhibitor)%euler_function == 1){
+                return close_exhibitor;
+            }
+            close_exhibitor ++;
+        }
+    }
+    private boolean simle_number(long number){
+        for (long j = 2; j < number; j++) {
+            if (number % j == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    private long open_exhibitor(){
+        long open_exhibitor = this.euler_function - 1;
+        //long euler_function = this.euler_function;
+        boolean flag = true;
+        ArrayList<Long> list_simple_number = new ArrayList<Long>(1000);
+
+        while (open_exhibitor > 1){
+            for (long i = 2; i <= euler_function; i++) {
+                if (open_exhibitor % i == 0 && euler_function % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag){
+                System.out.println("true");
+                if (simle_number(open_exhibitor)){
+                    list_simple_number.add(open_exhibitor);
+                }
+                System.out.println(list_simple_number);
+            }
+            flag = true;
+            open_exhibitor--;
+        }
+
+        Random r = new Random();
+        open_exhibitor = list_simple_number.get(r.nextInt(0, list_simple_number.size()));
+        System.out.println(open_exhibitor);
+        return open_exhibitor;
+    }
+    public Close_key get_close_key(){
+
+        return new Close_key(close_exhibitor, derivative);
+    }
+
+
+    public Open_key get_open_key(){
+        return new Open_key(open_exhibitor, derivative);
+    }
+
+
+
+}
--- src/main.java
+++ src/main.java
@@ -1,2 +1,33 @@
+import javax.management.BadAttributeValueExpException;
+import javax.management.ValueExp;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+        long first_prime_number = 0;
+        long second_prime_number = 0;
+        Scanner read = new Scanner(System.in);
+        try {
+            System.out.println("Введите первое простое число");
+            first_prime_number = read.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("ошибка в воде первого числа");
+            main a = new main();
+            System.exit(0);
+        }
+        try {
+            System.out.println("Введите второе простое число");
+            second_prime_number = read.nextInt();
+        }catch (InputMismatchException e){
+            System.out.println("ошибка в воде первого числа");
+            main a = new main();
+            System.exit(0);
+        }
+        key_generation key = new key_generation(first_prime_number, second_prime_number);
+        Close_key close_key = key.get_close_key();
+        Open_key open_key = key.get_open_key();
+        System.out.println(close_key);
+        System.out.println(open_key);
+    }
 }
